394319dd8d93d3655842e23d6af49b1a01914a9c
Роман Гуляев <r.v.gulyaev@yandex.ru>
2024-07-10 13:41:25 +0400

    Исправление ошибок в недельном отчете

--- app/Exports/WeekReportExport.php
+++ app/Exports/WeekReportExport.php
@@ -10,12 +10,12 @@ class WeekReportExport implements FromView
 {
     public function view(): View
     {
-        $startDate = date('Y-m-d', strtotime("last sunday midnight", strtotime("-1 week +1 day")));
-        $endDate = date("Y-m-d", strtotime('next saturday', strtotime("-1 week +1 day")));
-        $clients = DB::table('clients')->select(['clients.id', 'clients.fio'])->get();
-        $classes = DB::table('classes')->select(['classes.id as class_id', 'classes.name as class_name', 'classes.class_group_id', 'classes_groups.name as group_name'])->leftJoin('classes_groups', 'classes_groups.id', '=', 'classes.class_group_id')->get()->sortBy('class_group_id');
+        $startDate = date('Y-m-d', strtotime("last monday midnight", strtotime("-1 week")));
+        $endDate = date("Y-m-d", strtotime('next sunday', strtotime("-1 week")));
+        $clients = DB::table('clients')->select(['clients.id', 'clients.fio'])->whereNull('clients.deleted_at')->get()->sortBy('clients.fio');
+        $classes = DB::table('classes')->select(['classes.id as class_id', 'classes.name as class_name', 'classes.class_group_id', 'classes_groups.name as group_name'])->leftJoin('classes_groups', 'classes_groups.id', '=', 'classes.class_group_id')->whereNull('classes.deleted_at')->get()->sortBy('class_group_id');
         
-        $records = 0;
+     
         $clientdata = [];
         $headclasses = [];
         $weekdata = collect();
@@ -26,7 +26,7 @@ class WeekReportExport implements FromView
                 array_push($headclasses, ['class_id' => $class->class_id, 'class_name' => $class->class_name, 'group_id' => $class->class_group_id]);
             } else {
                 if($lastgroup !== $class->class_group_id) {
-                    array_push($headclasses, ['class_id' => $class->class_group_id, 'class_name' => $class->group_name, 'group_id' => $class->class_group_id]);
+                    array_push($headclasses, ['class_id' => $class->class_id, 'class_name' => $class->group_name, 'group_id' => $class->class_group_id]);
                     $lastgroup = $class->class_group_id;
                 }
             }
@@ -34,16 +34,16 @@ class WeekReportExport implements FromView
 
         $lastgroup = 0;
         foreach($clients as $client) {
-            foreach($headclasses as $class) { 
-                    $record = DB::table('record')->whereBetween('educationDate', [$startDate, $endDate])
-                    ->leftJoin('classes', 'classes.id', '=', 'record.class_id')->where('client_id', '=', $client->id)->where('classes.class_group_id', '=', $class['group_id'])->where('is_present', '=', 1)->count();
-                         
-                if ($lastgroup === $class['group_id'] && $class['group_id'] !== 0) {
-                    $records = $records + $record;
+            foreach($headclasses as $class) {
+
+                if ($class['group_id'] !== 0) {
+                    $record = DB::table('record')->leftJoin('classes', 'classes.id', '=', 'record.class_id')->leftJoin('classes_groups', 'classes_groups.id', '=', 'classes.class_group_id')->where('client_id', '=', $client->id)->whereBetween('educationDate', [$startDate, $endDate])
+                    ->where('classes.class_group_id', '=', $class['group_id'])->where('is_present', '=', 1)->count();
+                    array_push($clientdata, $record);
                 } else {
-                    array_push($clientdata, $records);
-                    $lastgroup = $class['group_id'];
-                    $records = $record;
+                    $record = DB::table('record')->leftJoin('classes', 'classes.id', '=', 'record.class_id')->where('client_id', '=', $client->id)->whereBetween('educationDate', [$startDate, $endDate])
+                    ->where('record.class_id', '=', $class['class_id'])->where('is_present', '=', 1)->count();
+                    array_push($clientdata, $record);
                 }
                               
             }
